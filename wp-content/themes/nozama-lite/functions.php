97dca4e81a51838c22958375d7320516ba54bd22
Blue Webmaster <bluehome88@outlook.com>
2021-07-12 23:23:00 +0300

    exclude subcategory products on category page

--- wp-content/themes/nozama-lite/functions.php
+++ wp-content/themes/nozama-lite/functions.php
@@ -553,3 +553,18 @@ if( isset($_GET['importer']) ){
 	}
 	exit;
 }
+
+function exclude_product_cat_children($wp_query) {
+if ( isset ( $wp_query->query_vars['product_cat'] ) && $wp_query->is_main_query()) {
+    $wp_query->set('tax_query', array(
+                                    array (
+                                        'taxonomy' => 'product_cat',
+                                        'field' => 'slug',
+                                        'terms' => $wp_query->query_vars['product_cat'],
+                                        'include_children' => false
+                                    )
+                                 )
+    );
+  }
+}
+add_filter('pre_get_posts', 'exclude_product_cat_children');
