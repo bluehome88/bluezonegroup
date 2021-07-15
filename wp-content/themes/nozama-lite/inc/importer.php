72799682d2f6ecd7e7f4f8af7e01993df23327ac
Blue Webmaster <bluehome88@outlook.com>
2021-07-15 16:59:09 +0300

    - Update importer to fix duplication except `New "RioPro" ` - ignore url replacing when import

--- wp-content/themes/nozama-lite/inc/importer.php
+++ wp-content/themes/nozama-lite/inc/importer.php
@@ -1,117 +1,4 @@
 <?php
-
-// Post Import from html file ( Latet News )
-if( isset($_GET['import_news']) ){
-	echo "Run script<br>";
-
-	$dom = new DOMDocument();
-	$arrPosts = array();
-
-	if( !empty($_GET['url']) ){
-		$title = $date = $link = "";
-		$link = $_GET['url'];
-
-		$dom->loadHTMLFile($link);
-		$documentElement = $dom->documentElement; 
-		$title = $dom->getElementsByTagName('h1');
-		$title = $title[0]->textContent;
-
-		$finder = new DomXPath($dom);
-		$classname = "date";
-		$nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
-		$children  = $nodes[0]->childNodes;
-		$date = $children[0]->textContent;
-
-		$arrPosts[] = array( "title"=>$title, "link"=> $link, "date" => $date );
-	}
-	else{
-		$dom->loadHTMLFile(get_theme_file_path('/news.html'));
-		$documentElement = $dom->documentElement; 
-		$h2s = $dom->getElementsByTagName('h2');
-		foreach( $h2s as $h2 ) {
-			$title = $date = $link = "";
-
-		    $child_elements = $h2->getElementsByTagName('span');
-			$link = $h2->getElementsByTagName('a');
-			$title = $link[0]->textContent;
-			$link = $link[0]->getAttribute('href');
-			$date = $child_elements[0]->textContent;
-			$arrPosts[] = array( "title"=>$title, "link"=> $link, "date" => $date );
-		}
-	}
-
-	$base_url = "https://www.bluezonegroup.com.au";
-	for( $i = count( $arrPosts )-1; $i >= 0; $i--){
-
-		$html_link = $base_url.$arrPosts[$i]['link'];
-		$dom->loadHTMLFile($html_link);
-		$finder = new DomXPath($dom);
-		$classname="announcement-details";
-		$nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
-
-		$innerHTML = ""; 
-	    $children  = $nodes[0]->childNodes;
-
-	    foreach ($children as $child) 
-	    {
-	    	$className = "";
-	    	if ($child->nodeType == XML_ELEMENT_NODE)
-	    		$className = $child->getAttribute('class');
-
-	    	if( $className == "date" || $className == "comment-form" || $className == "goBack")
-	    		continue;
-
-	        $innerHTML .= $nodes[0]->ownerDocument->saveHTML($child);
-	    }
-
-		$posts = get_posts([
-		    'title' => $arrPosts[$i]['title'],
-		]);
-
-		/* ---- store to DB ---- */
-
-		// adjust news url
-        $innerHTML = str_replace('https://www.bluezonegroup.com.au/announcements', 'https://bluezonegroup.rbdev.com.au/announcements', $innerHTML);
-
-        // adjust youtube url
-        if( strpos($innerHTML, '//www.youtube') !== false)
-        	$innerHTML = str_replace('src="//www.youtube', 'src="https://www.youtube', $innerHTML);
-
-        // adjust image url
-        $innerHTML = str_replace('src="/', 'src="https://www.bluezonegroup.com.au/', $innerHTML);
-
-        // Fix PDF link issue
-        if( strpos($innerHTML, '/Literature') !== false)
-        	$innerHTML = str_replace('href="/Literature', 'href="https://www.bluezonegroup.com.au/Literature', $innerHTML);
-
-        if( strpos($innerHTML, '/_literature') !== false)
-        	$innerHTML = str_replace('href="/_literature', 'href="https://www.bluezonegroup.com.au/_literature', $innerHTML);
-
-        $post_id = null;
-		if(!empty($posts) && !isset($_GET['url']) ){
-			$post_id = $posts[0]->ID;
-			wp_update_post( array("ID"=>$post_id, "post_content"=>$innerHTML, 'post_status'   => 'publish'));
-			echo ++$index.":Updated <a href='".get_permalink($post_id)."'>".$arrPosts[$i]['title']."</a><br><br>";
-		}
-		else
-		{
-			$my_post = array(
-			  'post_title'    => $arrPosts[$i]['title'],
-			  'post_content'  => $innerHTML,
-			  'post_status'   => 'publish',
-			  'post_author'   => 1,
-			  'post_category' => array( 1 ),
-			  'post_date'	  => date( 'Y-m-d H:i:s', strtotime($arrPosts[$i]['date']))
-			);
-
-			// Insert the post into the database
-			$post_id = wp_insert_post( $my_post );			
-			echo ++$index.":New Post <a href='".get_permalink($post_id)."'>".$arrPosts[$i]['title']."</a><br><br>";
-		}
-	}
-	exit;
-}
-
 // import products
 if( isset($_GET['import_product']) ){
 	echo "Run script<br>";
@@ -224,6 +111,122 @@ print_r( $arrRes );
 	exit;
 }
 
+
+// Post Import from html file ( Latet News )
+if( isset($_GET['import_news']) ){
+	echo "Run script<br>";
+
+	$dom = new DOMDocument();
+	$arrPosts = array();
+
+	if( !empty($_GET['url']) ){
+		$title = $date = $link = "";
+		$link = $_GET['url'];
+
+		$dom->loadHTMLFile($link);
+		$documentElement = $dom->documentElement; 
+		$title = $dom->getElementsByTagName('h1');
+		$title = $title[0]->textContent;
+
+		$finder = new DomXPath($dom);
+		$classname = "date";
+		$nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
+		$children  = $nodes[0]->childNodes;
+		$date = $children[0]->textContent;
+
+		$arrPosts[] = array( "title"=>$title, "link"=> $link, "date" => $date );
+	}
+	else{
+		$dom->loadHTMLFile(get_theme_file_path('/news.html'));
+		$documentElement = $dom->documentElement; 
+		$h2s = $dom->getElementsByTagName('h2');
+		foreach( $h2s as $h2 ) {
+			$title = $date = $link = "";
+
+		    $child_elements = $h2->getElementsByTagName('span');
+			$link = $h2->getElementsByTagName('a');
+			$title = $link[0]->textContent;
+			$link = $link[0]->getAttribute('href');
+			$date = $child_elements[0]->textContent;
+			$arrPosts[] = array( "title"=>$title, "link"=> $link, "date" => $date );
+		}
+	}
+
+	$base_url = "https://www.bluezonegroup.com.au";
+	for( $i = count( $arrPosts )-1; $i >= 0; $i--){
+
+		$html_link = $base_url.$arrPosts[$i]['link'];
+		$dom->loadHTMLFile($html_link);
+		$finder = new DomXPath($dom);
+		$classname="announcement-details";
+		$nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
+
+		$innerHTML = ""; 
+	    $children  = $nodes[0]->childNodes;
+
+	    foreach ($children as $child) 
+	    {
+	    	$className = "";
+	    	if ($child->nodeType == XML_ELEMENT_NODE)
+	    		$className = $child->getAttribute('class');
+
+	    	if( $className == "date" || $className == "comment-form" || $className == "goBack")
+	    		continue;
+
+	        $innerHTML .= $nodes[0]->ownerDocument->saveHTML($child);
+	    }
+
+		$posts = get_posts([
+		    'title' => htmlspecialchars($arrPosts[$i]['title']),
+		]);
+
+		/* ---- store to DB ---- */
+
+		/*// adjust news url
+        $innerHTML = str_replace('https://www.bluezonegroup.com.au/announcements', 'https://bluezonegroup.rbdev.com.au/announcements', $innerHTML);
+
+        // adjust youtube url
+        if( strpos($innerHTML, '//www.youtube') !== false)
+        	$innerHTML = str_replace('src="//www.youtube', 'src="https://www.youtube', $innerHTML);
+
+        // adjust image url
+        $innerHTML = str_replace('src="/', 'src="https://www.bluezonegroup.com.au/', $innerHTML);
+
+        // Fix PDF link issue
+        if( strpos($innerHTML, '/Literature') !== false)
+        	$innerHTML = str_replace('href="/Literature', 'href="https://www.bluezonegroup.com.au/Literature', $innerHTML);
+
+        if( strpos($innerHTML, '/_literature') !== false)
+        	$innerHTML = str_replace('href="/_literature', 'href="https://www.bluezonegroup.com.au/_literature', $innerHTML);
+		/**/
+
+        $post_id = null;
+		if(!empty($posts) && !isset($_GET['url']) ){
+			$post_id = $posts[0]->ID;
+			// wp_update_post( array("ID"=>$post_id, "post_content"=>$innerHTML, 'post_status'   => 'publish'));
+			// echo ++$index.":Updated <a href='".get_permalink($post_id)."'>".$arrPosts[$i]['title']."</a><br><br>";
+		}
+		else
+		{
+			$my_post = array(
+			  'post_title'    => $arrPosts[$i]['title'],
+			  'post_content'  => $innerHTML,
+			  'post_status'   => 'publish',
+			  'post_author'   => 1,
+			  'post_category' => array( 1 ),
+			  'post_date'	  => date( 'Y-m-d H:i:s', strtotime($arrPosts[$i]['date']))
+			);
+
+			// Insert the post into the database
+			$post_id = wp_insert_post( $my_post );			
+			echo ++$index.":New Post <a href='".get_permalink($post_id)."'>".$arrPosts[$i]['title']."</a><br><br>";
+		}
+	}
+	exit;
+}
+
+// Check issued news
+// Important: only run once for updating
 if( isset($_GET['issued_news']) ){
 	wp_reset_query();
 
@@ -234,34 +237,100 @@ if( isset($_GET['issued_news']) ){
 	]);
 
 	$arrRes = array();
-	foreach( $posts as $product ){
+	
+	$old_domain = "www.bluezonegroup.com.au";
+	$new_domain = "bluezonegroup.rbdev.com.au";
+	
+	foreach( $posts as $post ){
 
+		$content = $post->post_content;
+
+/*
 		// check empty contents
-		if( $product->post_content == "" ){
-			$arrRes['empty_content'][] = "<a href='".get_permalink($product->ID)."'>".$product->post_title."</a><br>";
+		if( $content == "" ){
+			$arrRes['empty_content'][] = "<a href='".get_permalink($post->ID)."'>".$post->post_title."</a><br>";
 		}
+*/
 
-		if( strpos($product->post_content, '/announcements/') !== false){
-			$arrRes['has_news_link'][] = "<a href='".get_permalink($product->ID)."'>".$product->post_title."</a><br>";
+/*
+		// fix product link
+		if( strpos($content, $old_domain.'/product-catalogue/') !== false || strpos($content, $old_domain.'/rental-equipment/') !== false ){
+			$arrRes['has_product_link'][] = "<a href='".get_permalink($post->ID)."'>".$post->post_title."</a><br>";
+			$content = str_replace($old_domain.'/product-catalogue/', 'bluezonegroup.rbdev.com.au/product-catalogue/', $content);
+			$content = str_replace($old_domain.'/rental-equipment/', 'bluezonegroup.rbdev.com.au/rental-equipment/', $content);
+			// wp_update_post( array("ID"=>$post->ID, "post_content"=>$content));
 		}
-		if( strpos($product->post_content, '/Literature') !== false){
-			$arrRes['has_pdf_link'][] = "<a href='".get_permalink($product->ID)."'>".$product->post_title."</a><br>";
-			// $content = str_replace('href="/Literature', 'href="https://www.bluezonegroup.com.au/Literature', $product->post_content);
-			// wp_update_post( array("ID"=>$product->ID, "post_content"=>$content));
+*/
+
+/*
+		// Fix news link
+		if( strpos($content, $old_domain.'/announcements') !== false){
+			$arrRes['has_news_link'][] = "<a href='".get_permalink($post->ID)."'>".$post->post_title."</a><br>";
+			$content = str_replace($old_domain.'/announcements', 'bluezonegroup.rbdev.com.au/announcements', $content);
+			// wp_update_post( array("ID"=>$post->ID, "post_content"=>$content));
 		}
-		if( strpos($product->post_content, 'Literature/pdf.png') !== false){
-			$arrRes['has_pdf_link_with_icon'][] = "<a href='".get_permalink($product->ID)."'>".$product->post_title."</a><br>";
-			// $content = str_replace('href="/_literature', 'href="https://www.bluezonegroup.com.au/_literature', $product->post_content);
-			// wp_update_post( array("ID"=>$product->ID, "post_content"=>$content));
+*/
+
+/*
+		if( strpos($content, 'http://www.uvs.com.au/') !== false){
+			$arrRes['has_old_link'][] = "<a href='".get_permalink($post->ID)."'>".$post->post_title."</a><br>";
+			$content = str_replace('http://www.uvs.com.au/', 'https://bluezonegroup.rbdev.com.au/', $content);
+			// wp_update_post( array("ID"=>$post->ID, "post_content"=>$content));
+		}		
+*/
+
+/*
+		if( strpos($content, '/services/') !== false){
+			$arrRes['has_service_link'][] = "<a href='".get_permalink($post->ID)."'>".$post->post_title."</a><br>";
+			$content = str_replace($old_domain.'/services', '/services', $content);
+			$content = str_replace('https:///services', '/services', $content);
+			// wp_update_post( array("ID"=>$post->ID, "post_content"=>$content));
 		}
-		if( strpos($product->post_content, 'youtube') !== false){
-			$arrRes['has_video_youtuve'][] = "<a href='".get_permalink($product->ID)."'>".$product->post_title."</a><br>";
-			// wp_update_post( array("ID"=>$product->ID, "post_content"=>$content));
+*/
+/*
+		if( strpos($content, '/Literature') !== false && strpos($content, '/Literature') !== false){
+			$arrRes['has_pdf_link'][] = "<a href='".get_permalink($post->ID)."'>".$post->post_title."</a><br>";
+			$content = str_replace('href="/Literature', 'href="https://www.bluezonegroup.com.au/Literature', $content);
+			// wp_update_post( array("ID"=>$post->ID, "post_content"=>$content));
 		}
+*/
+/*
+		if( strpos($content, '/_literature') !== false){
+			$arrRes['has_pdf_link_with_icon'][] = "<a href='".get_permalink($post->ID)."'>".$post->post_title."</a><br>";
+			$content = str_replace('href="/_literature', 'href="https://www.bluezonegroup.com.au/_literature', $content);
+			// wp_update_post( array("ID"=>$post->ID, "post_content"=>$content));
+		}
+*/
+/*
+		if( strpos($content, '//www.youtube') !== false){
+			$arrRes['has_video_youtube'][] = "<a href='".get_permalink($post->ID)."'>".$post->post_title."</a><br>";
+			$content = str_replace('//www.youtube', 'src="https://www.youtube', $content);
+			// wp_update_post( array("ID"=>$post->ID, "post_content"=>$content));
+		}
+*/
+		
+/*
+		// Fix Image issue
+		if( strpos($content, 'src="/') !== false){
+			$arrRes['without_baselink'][] = "<a href='".get_permalink($post->ID)."'>".$post->post_title."</a><br>";
+			$content = str_replace('src="/', 'src="https://www.bluezonegroup.com.au/', $content);
+			// wp_update_post( array("ID"=>$post->ID, "post_content"=>$content));
+		}
+*/
+		
+		// wp_update_post( array("ID"=>$post->ID, "post_content"=>$content));
 	}
 
 echo "<pre>";
-print_r( $arrRes );
+// print_r( $arrRes );
+
+	foreach( $arrRes as $key => $arrType )
+	{
+		echo $key."-----------------------------------------------------------------<br>";
+		foreach( $arrType as $post ){
+			echo "            ".$post."<br>";
+		}
+	}
 
 	exit;
 }
