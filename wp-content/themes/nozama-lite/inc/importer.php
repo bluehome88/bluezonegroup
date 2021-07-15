ddce326fe838dba2e4cbffd4a51ef6d123aabde9
Blue Webmaster <bluehome88@outlook.com>
2021-07-15 12:54:17 +0300

    force import for individual news by url

--- wp-content/themes/nozama-lite/inc/importer.php
+++ wp-content/themes/nozama-lite/inc/importer.php
@@ -1,25 +1,43 @@
 <?php
 
 // Post Import from html file ( Latet News )
-if( isset($_GET['importer']) ){
+if( isset($_GET['import_news']) ){
 	echo "Run script<br>";
 
 	$dom = new DOMDocument();
-	$dom->loadHTMLFile(get_theme_file_path('/news.html'));
-	$documentElement = $dom->documentElement; 
-
 	$arrPosts = array();
-	$h2s = $dom->getElementsByTagName('h2');
-	foreach( $h2s as $h2 ) {
+
+	if( !empty($_GET['url']) ){
 		$title = $date = $link = "";
+		$link = $_GET['url'];
 
-	    $child_elements = $h2->getElementsByTagName('span');
-		$link = $h2->getElementsByTagName('a');
-		$title = $link[0]->textContent;
-		$link = $link[0]->getAttribute('href');
-		$date = $child_elements[0]->textContent;
+		$dom->loadHTMLFile($link);
+		$documentElement = $dom->documentElement; 
+		$title = $dom->getElementsByTagName('h1');
+		$title = $title[0]->textContent;
+
+		$finder = new DomXPath($dom);
+		$classname = "date";
+		$nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
+		$children  = $nodes[0]->childNodes;
+		$date = $children[0]->textContent;
+
 		$arrPosts[] = array( "title"=>$title, "link"=> $link, "date" => $date );
+	}
+	else{
+		$dom->loadHTMLFile(get_theme_file_path('/news.html'));
+		$documentElement = $dom->documentElement; 
+		$h2s = $dom->getElementsByTagName('h2');
+		foreach( $h2s as $h2 ) {
+			$title = $date = $link = "";
 
+		    $child_elements = $h2->getElementsByTagName('span');
+			$link = $h2->getElementsByTagName('a');
+			$title = $link[0]->textContent;
+			$link = $link[0]->getAttribute('href');
+			$date = $child_elements[0]->textContent;
+			$arrPosts[] = array( "title"=>$title, "link"=> $link, "date" => $date );
+		}
 	}
 
 	$base_url = "https://www.bluezonegroup.com.au";
@@ -50,11 +68,33 @@ if( isset($_GET['importer']) ){
 		    'title' => $arrPosts[$i]['title'],
 		]);
 
-		// store to DB
-		echo "No ".++$index.": ".$arrPosts[$i]['title']."<br>";
-		if(!empty($posts))
-			wp_update_post( array("ID"=>$posts[0]->ID, "post_content"=>$innerHTML));
-		else{
+		/* ---- store to DB ---- */
+
+		// adjust news url
+        $innerHTML = str_replace('https://www.bluezonegroup.com.au/announcements', 'https://bluezonegroup.rbdev.com.au/announcements', $innerHTML);
+
+        // adjust youtube url
+        if( strpos($innerHTML, '//www.youtube') !== false)
+        	$innerHTML = str_replace('src="//www.youtube', 'src="https://www.youtube', $innerHTML);
+
+        // adjust image url
+        $innerHTML = str_replace('src="/', 'src="https://www.bluezonegroup.com.au/', $innerHTML);
+
+        // Fix PDF link issue
+        if( strpos($innerHTML, '/Literature') !== false)
+        	$innerHTML = str_replace('href="/Literature', 'href="https://www.bluezonegroup.com.au/Literature', $innerHTML);
+
+        if( strpos($innerHTML, '/_literature') !== false)
+        	$innerHTML = str_replace('href="/_literature', 'href="https://www.bluezonegroup.com.au/_literature', $innerHTML);
+
+        $post_id = null;
+		if(!empty($posts) && !isset($_GET['url']) ){
+			$post_id = $posts[0]->ID;
+			wp_update_post( array("ID"=>$post_id, "post_content"=>$innerHTML, 'post_status'   => 'publish'));
+			echo ++$index.":Updated <a href='".get_permalink($post_id)."'>".$arrPosts[$i]['title']."</a><br><br>";
+		}
+		else
+		{
 			$my_post = array(
 			  'post_title'    => $arrPosts[$i]['title'],
 			  'post_content'  => $innerHTML,
@@ -63,8 +103,10 @@ if( isset($_GET['importer']) ){
 			  'post_category' => array( 1 ),
 			  'post_date'	  => date( 'Y-m-d H:i:s', strtotime($arrPosts[$i]['date']))
 			);
+
 			// Insert the post into the database
-			wp_insert_post( $my_post );			
+			$post_id = wp_insert_post( $my_post );			
+			echo ++$index.":New Post <a href='".get_permalink($post_id)."'>".$arrPosts[$i]['title']."</a><br><br>";
 		}
 	}
 	exit;
@@ -115,7 +157,6 @@ if( isset($_GET['import_product']) ){
 
         if( strpos($innerHTML, '/_literature') !== false)
         	$innerHTML = str_replace('href="/_literature', 'href="https://www.bluezonegroup.com.au/_literature', $innerHTML);
-
     }
 
 	echo $product_name."<br>".$innerHTML;
@@ -148,6 +189,48 @@ if( isset($_GET['issued_product']) ){
 	$posts = get_posts([
 	    'post_type' => 'product',
 	    'posts_per_page' => 500
+	]);
+
+	$arrRes = array();
+	foreach( $posts as $product ){
+
+		// check empty contents
+		if( $product->post_content == "" ){
+			$arrRes['empty_content'][] = "<a href='".get_permalink($product->ID)."'>".$product->post_title."</a><br>";
+		}
+
+		if( strpos($product->post_content, '/announcements/') !== false){
+			$arrRes['has_news_link'][] = "<a href='".get_permalink($product->ID)."'>".$product->post_title."</a><br>";
+		}
+		if( strpos($product->post_content, '/Literature') !== false){
+			$arrRes['has_pdf_link'][] = "<a href='".get_permalink($product->ID)."'>".$product->post_title."</a><br>";
+			// $content = str_replace('href="/Literature', 'href="https://www.bluezonegroup.com.au/Literature', $product->post_content);
+			// wp_update_post( array("ID"=>$product->ID, "post_content"=>$content));
+		}
+		if( strpos($product->post_content, 'Literature/pdf.png') !== false){
+			$arrRes['has_pdf_link_with_icon'][] = "<a href='".get_permalink($product->ID)."'>".$product->post_title."</a><br>";
+			// $content = str_replace('href="/_literature', 'href="https://www.bluezonegroup.com.au/_literature', $product->post_content);
+			// wp_update_post( array("ID"=>$product->ID, "post_content"=>$content));
+		}
+		if( strpos($product->post_content, 'youtube') !== false){
+			$arrRes['has_video_youtuve'][] = "<a href='".get_permalink($product->ID)."'>".$product->post_title."</a><br>";
+			// wp_update_post( array("ID"=>$product->ID, "post_content"=>$content));
+		}
+	}
+
+echo "<pre>";
+print_r( $arrRes );
+
+	exit;
+}
+
+if( isset($_GET['issued_news']) ){
+	wp_reset_query();
+
+	$posts = get_posts([
+	    'post_type' => 'post',
+	    'posts_per_page' => 500,
+	    'cat' => 1
 	]);
 
 	$arrRes = array();
