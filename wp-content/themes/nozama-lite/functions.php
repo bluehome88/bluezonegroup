7590ea9c52941978632a07d18ce664f60973d0f9
Blue Webmaster <bluehome88@outlook.com>
2021-08-02 11:18:14 +0300

    highlight active menu item

--- wp-content/themes/nozama-lite/functions.php
+++ wp-content/themes/nozama-lite/functions.php
@@ -551,3 +551,14 @@ function woocommerce_show_product_subcategories() {
 
 // include importer function
 require_once get_theme_file_path( '/inc/importer.php' );
+
+add_filter('widget_text', 'do_shortcode');
+
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+function special_nav_class ($classes, $item) {
+  if (in_array('current-menu-item', $classes) ){
+    $classes[] = 'active ';
+  }
+  return $classes;
+}
