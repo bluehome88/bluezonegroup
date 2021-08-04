de5fb7a9b53ad6fe6d64d08ff9dadcf798d74cba
Blue Webmaster <bluehome88@outlook.com>
2021-08-05 00:46:20 +0300

    page template to Elementor full width

--- wp-content/themes/nozama-lite/page.php
+++ wp-content/themes/nozama-lite/page.php
@@ -1,43 +1,80 @@
-<?php get_header(); ?>
+<?php
 
-<main class="main">
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
 
-	<div class="container">
+if( is_plugin_active(plugin_dir_path( __DIR__ ) . 'elementor') )
+{
+	\Elementor\Plugin::$instance->frontend->add_body_class( 'elementor-template-full-width' );
 
-		<div class="row <?php nozama_lite_the_row_classes(); ?>">
+	get_header();
+	/**
+	 * Before Header-Footer page template content.
+	 *
+	 * Fires before the content of Elementor Header-Footer page template.
+	 *
+	 * @since 2.0.0
+	 */
+	do_action( 'elementor/page_templates/header-footer/before_content' );
 
-			<?php get_template_part( 'template-parts/breadcrumbs' ); ?>
+	\Elementor\Plugin::$instance->modules_manager->get_modules( 'page-templates' )->print_content();
 
-			<div id="site-content" class="<?php nozama_lite_the_container_classes(); ?>">
+	/**
+	 * After Header-Footer page template content.
+	 *
+	 * Fires after the content of Elementor Header-Footer page template.
+	 *
+	 * @since 2.0.0
+	 */
+	do_action( 'elementor/page_templates/header-footer/after_content' );
 
-				<?php while ( have_posts() ) : the_post(); ?>
+	get_footer();
+}
+else
+{
+	get_header(); ?>
 
-					<article id="entry-<?php the_ID(); ?>" <?php post_class( 'entry' ); ?>>
+	<main class="main">
 
-						<?php nozama_lite_the_post_thumbnail(); ?>
+		<div class="container">
 
-						<?php nozama_lite_the_post_header(); ?>
+			<div class="row <?php nozama_lite_the_row_classes(); ?>">
 
-						<div class="entry-content">
-							<?php the_content(); ?>
+				<?php get_template_part( 'template-parts/breadcrumbs' ); ?>
 
-							<?php wp_link_pages( nozama_lite_wp_link_pages_default_args() ); ?>
-						</div>
+				<div id="site-content" class="<?php nozama_lite_the_container_classes(); ?>">
 
-					</article>
+					<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php comments_template(); ?>
+						<article id="entry-<?php the_ID(); ?>" <?php post_class( 'entry' ); ?>>
 
-				<?php endwhile; ?>
+							<?php nozama_lite_the_post_thumbnail(); ?>
+
+							<?php nozama_lite_the_post_header(); ?>
+
+							<div class="entry-content">
+								<?php the_content(); ?>
+
+								<?php wp_link_pages( nozama_lite_wp_link_pages_default_args() ); ?>
+							</div>
+
+						</article>
+
+						<?php comments_template(); ?>
+
+					<?php endwhile; ?>
+
+				</div>
+
+				<?php get_sidebar(); ?>
 
 			</div>
 
-			<?php get_sidebar(); ?>
-
 		</div>
 
-	</div>
+	</main>
 
-</main>
+	<?php get_footer();
 
-<?php get_footer();
+}
