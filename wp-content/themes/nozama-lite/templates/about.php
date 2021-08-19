c688e40a99b28358c35579aca9458b96df1b6ec3
Blue Webmaster <bluehome88@outlook.com>
2021-08-19 06:24:40 +0300

    adjust about page template

--- wp-content/themes/nozama-lite/templates/about.php
+++ wp-content/themes/nozama-lite/templates/about.php
@@ -8,10 +8,20 @@ get_header(); ?>
 <main class="landing">
 
     <?php the_content(); ?>
-    <div class="container">
+    <div class="gallery">
+        <div class="container">
         <?php the_widget('WP_Widget_Media_Gallery', 'title=Gallery' ); ?>
+        </div>
+    </div>
+    <div class="nozama_featured_product">
+        <div class="container">
         <?php the_widget( 'CI_Widget_Home_Latest_Products', 'title=Related Products' ); ?>
+        </div>
+    </div>
+    <div class="nozama_news">
+        <div class="container">
         <?php the_widget( 'CI_Widget_Latest_Posts', 'title=Related articles' ); ?>
+        </div>
     </div>
 </main>
 
